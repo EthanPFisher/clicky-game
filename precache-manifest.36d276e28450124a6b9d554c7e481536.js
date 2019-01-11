63029181dd132df4dc2a5e1ee13f5c0e1d4bdf05
Loafus <eth.fish@gmail.com>
2019-01-10 21:14:43 -0800

    Updates

--- precache-manifest.36d276e28450124a6b9d554c7e481536.js
+++ precache-manifest.36d276e28450124a6b9d554c7e481536.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
   },
   {
-    "revision": "97531697f963bff6ffba",
-    "url": "/clicky-game/static/js/main.97531697.chunk.js"
+    "revision": "ec835825e497441fe4b7",
+    "url": "/clicky-game/static/js/main.ec835825.chunk.js"
   },
   {
     "revision": "fd4ba84bba80243ecffc",
     "url": "/clicky-game/static/js/1.fd4ba84b.chunk.js"
   },
   {
-    "revision": "97531697f963bff6ffba",
+    "revision": "ec835825e497441fe4b7",
     "url": "/clicky-game/static/css/main.de679c99.chunk.css"
   },
   {
-    "revision": "470c5aec98baf8aa652a5e733bcbf3d0",
+    "revision": "2c840bb8f9d5831e215234fa7f4608c1",
     "url": "/clicky-game/index.html"
   }
 ];
